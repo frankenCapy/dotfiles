dd2b2a442f46481b477ba167a12b377838ab12f0
Simon Pfannenmueller <simon.pfannenmueller@astrazeneca.com>
2024-06-10 18:15:24 +0200

    add copilot and chat

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+        layout = {
+          position = "bottom",
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-e>",
+          prev = "<M-n>",
+          dismiss = "<M-x>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = 'node',
+      server_opts_overrides = {},
+    }
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = {
+      debug = true,
+    },
+  },
+}
